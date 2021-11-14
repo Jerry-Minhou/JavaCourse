154d19b54246e49acc32525cd5f30e98563b11fa
Jerry-Minhou <56499518+Jerry-Minhou@users.noreply.github.com>
2021-11-14 14:07:25 +0800

    Add files via upload

---
+++ Week02/httpclienttest/src/test/java/com/rjkf/test/httpclienttest/HttpclienttestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rjkf.test.httpclienttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpclienttestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
